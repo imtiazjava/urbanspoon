23c7098cd0bfb5ce5ffe769f4657f1403c9eaafe
imtiazjava <imtiazjavatrainer@gmail.com>
2018-02-10 18:09:40 +0530

    Add files via upload

---
+++ UploadPro.java
@@ -0,0 +1,46 @@
+package com.ts;
+
+public class UploadPro {
+	
+	private int id;
+	private String name;
+	private String imageName;
+	
+	
+	public UploadPro(){
+		System.out.println("**UploadPro object is created...");
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getImageName() {
+		return imageName;
+	}
+
+
+	public void setImageName(String imageName) {
+		this.imageName = imageName;
+	}
+	
+	
+
+}
